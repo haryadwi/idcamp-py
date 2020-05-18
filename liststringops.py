157de7531bea5678f4a090eb303cc107d36fc6c4
haryadwi <haryadwi@gmail.com>
2020-05-18 16:40:29 +0700

    Replikasi String

--- liststringops.py
+++ liststringops.py
@@ -1,12 +1,17 @@
-l = [1,2,3,3,4,4,4,4,5,6]
-s = set(l)
+'''
+l = [1, 2, 3, 3, 4, 4, 4, 4, 5, 6]  # tipe data list
+s = set(l)  # menjadikan tipe data set, saat dii pront tidak ada angka yang sama
 x = "Hello, World"
- 
+
 print(l)
-print(len(l))
- 
+print(len(l))  # menghitung jumlah list
+
 print(s)
-print(len(s))
- 
+print(len(s))  # menghitung jumlah set
+
 print(x)
-print(len(x))
+print(len(x))  # menghitung jumlah string pada x
+'''
+
+# Penggabungan dan replikasi pada list
+print([1, 2, 3] + ['A', 'B', 'C'])
