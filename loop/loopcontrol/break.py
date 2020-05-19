74146b58970c645229606998ad2a12dd0b624987
haryadwi <haryadwi@gmail.com>
2020-05-19 07:43:21 +0700

    Add 2nd example

--- loop/loopcontrol/break.py
+++ loop/loopcontrol/break.py
@@ -1,4 +1,14 @@
 for letter in 'Python':
-  if letter == 'h':
-    break
-  print("Current letter: {}".format(letter))
+    if letter == 'h':
+        break
+    print("Current letter: {}".format(letter))
+
+print('\n')
+
+var = 10
+
+while var > 0:
+    print("Current variable value: {}".format(var))
+    var = var - 1
+    if var == 3:
+      break
