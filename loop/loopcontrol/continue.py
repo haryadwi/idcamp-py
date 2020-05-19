10f515a3c17baff7d1e0534ea2a5ae14e7bec278
haryadwi <haryadwi@gmail.com>
2020-05-19 07:56:46 +0700

    Continue final commit

--- loop/loopcontrol/continue.py
+++ loop/loopcontrol/continue.py
@@ -1,4 +1,21 @@
 for letter in 'Python':
-  if letter == 'h': # Huruf h akan dilewat saat perulangan
+    if letter == 'h':  # Huruf h akan dilewat saat perulangan
+        continue
+    print("Current letter: {}".format(letter))
+
+print('\n')
+
+for a in [0, 1, -1, 2, -2, 3, -3]:
+    if a <= 0:  # Bilangan negatif pada list tidak akan diulang
+        continue    
+    print("Elemen Positif: {}".format(a))
+
+print('\n')
+
+var = 10
+
+while var > 0:
+  var = var - 1
+  if var == 5: # Angka 5 akan di lewat saat pengulangan
     continue
-  print("Current letter: {}".format(letter))
+  print("Current variable value: {}".format(var))
