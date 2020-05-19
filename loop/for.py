7aa1f8362fb670e3844bb57609458a093ca34cce
haryadwi <haryadwi@gmail.com>
2020-05-19 07:15:46 +0700

    For first commit

---
+++ loop/for.py
@@ -0,0 +1,8 @@
+for letter in 'Python':
+    print("Current letter: {}".format(letter))
+
+print('\n')
+
+basket = ['Grape', 'Berry', 'Orange']
+for fruit in basket: 
+  print("Fruit in basket: {}".format(fruit))
