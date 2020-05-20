7b482d8d2905055d4d8ce822e156aac92e4bd04a
haryadwi <haryadwi@gmail.com>
2020-05-20 21:48:19 +0700

    Varlength final commit

--- function/varlengtharg.py
+++ function/varlengtharg.py
@@ -1,8 +1,26 @@
-def printinfo(fixedarg, *args): # *args merupakan variable penampung untunk inputan tuple
-  print("Output: fixedrag {}".format(fixedarg))
-  for a in args:
-    print("Argumen posisi {}".format(a))
+def printinfo(fixedarg, *args):  # *args merupakan variable penampung untunk inputan tuple
+    print("Output: fixedarg {}".format(fixedarg))
+    for a in args:
+        print("Argumen posisi {}".format(a))
+
 
 #  Pemanggilan fungsi
-printinfo(70)
+printinfo(10)
 printinfo(70, 60, 50)
+
+print('\n')
+
+
+def printdata(*args, **kwargs):
+    for a in args:
+        print("Argumen posisi {}".format(a))
+    for key, value in kwargs.items(): # Untuk memasukan dictioanry
+        print("Argumen kata kunci {}: {}".format(key, value))
+
+
+#  Pemanggilan fungsi
+printdata()
+printdata(1, 2, 3)
+printdata(x=7, y=8, z=9)
+printdata(1, 2, y=8, z=9)
+printdata(*(2, 3), **{'x':7, 'y':8})
