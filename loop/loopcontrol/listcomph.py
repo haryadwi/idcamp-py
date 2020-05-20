28c5a6855131287c5fa3bce36b2310be19fe6d3f
haryadwi <haryadwi@gmail.com>
2020-05-20 11:38:05 +0700

    List comperhension commit

---
+++ loop/loopcontrol/listcomph.py
@@ -0,0 +1,32 @@
+# traditional list loop insert
+numbers = [1, 2, 3, 4]
+square = []
+for n in numbers:
+  square.append(n**2) # membuat isi list number dipangkatkan lalu di input ke list square
+print(square)
+
+print('\n')
+
+# List comprehension insert (syntax: new_list = [expression for_loop_one_or_more conditions] )
+numbers = [1, 2, 3, 4]
+square = [n**2 for n in numbers] # Membuat isi list square berdasar isi list number yg dipangkatkan, lebih ringkas
+print(square)
+
+# Search the same numbers in two different list (traditional method)
+list_a = [1, 2, 3, 4]
+list_b = [2, 3, 4, 5]
+common_num = []
+
+for a in list_a:
+  for b in list_b:
+    if a == b:
+      common_num.append(a)
+
+print(common_num)
+
+# With list comperhension loop search
+list_a = [1, 2, 3, 4]
+list_b = [2, 3, 4, 5]
+common_num = [a for a in list_a for b in list_b if a==b] # Lebih ringkas
+print(common_num)
+
