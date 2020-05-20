074e22fbf06c71290c6fab6573ae64af45a83f90
haryadwi <haryadwi@gmail.com>
2020-05-20 17:19:49 +0700

    Except Handling Commit

--- ercepthandling/exchandling.py
+++ ercepthandling/exchandling.py
@@ -8,17 +8,38 @@ ZeroDivisionError: division by zero
 '''
 
 try:
-  x = 1 / z
-  print(x)
+    x = 1 / z
+    print(x)
 except ZeroDivisionError:
-  print("Tidak bisa membagi angka dengan nilai Nol")
+    print("Tidak bisa membagi angka dengan nilai Nol")
 
 print('\n')
 
 try:
   with open('tapi_bohong.py') as file:
     print(file.read())
-except (FileNotFoundError):
-  print("File Tidak Ditemukan")
+except (FileNotFoundError) as em:
+    print("File Tidak Ditemukan")
+    print("Pesan Sistem: {}".format(em))
 
+print('\n')
+
+d = {'ratarata': '10.0'}
+try:
+    print('rata-rata: {}'.format(d['rata_rata']))
+except KeyError:
+    print('kunci tidak ditemukan di dictionary')
+except ValueError:
+    print('nilai tidak sesuai')
+
+try:
+    print('rata-rata: {}'.format(d['ratarata']/3))
+except KeyError:
+    print('kunci tidak ditemukan di dictionary')
+except (ValueError, TypeError):
+    print('nilai atau tipe tidak sesuai')
+try:
+    print('pembulatan rata-rata: {}'.format(int(d['ratarata'])))
+except (ValueError, TypeError) as e:
+    print('penangan kesalahan: {}'.format(e))
 
