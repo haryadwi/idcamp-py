776a9553034db310bb7485ab3ffcac0545e731b6
haryadwi <haryadwi@gmail.com>
2020-05-20 17:09:36 +0700

    trial

--- ercepthandling/exchandling.py
+++ ercepthandling/exchandling.py
@@ -12,3 +12,13 @@ try:
   print(x)
 except ZeroDivisionError:
   print("Tidak bisa membagi angka dengan nilai Nol")
+
+print('\n')
+
+try:
+  with open('tapi_bohong.py') as file:
+    print(file.read())
+except (FileNotFoundError):
+  print("File Tidak Ditemukan")
+
+
