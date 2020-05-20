c6630da8b5f7365bbf15b342aed6ea91e920c939
haryadwi <haryadwi@gmail.com>
2020-05-20 20:36:54 +0700

    Argument function

--- function/argfunc.py
+++ function/argfunc.py
@@ -4,4 +4,18 @@ def printinfo(name, age):
     print("Name: ", name)
     print("Age: ", age)
 
+
 printinfo(age=24, name="Masjaw")
+
+print('\n')
+
+# Fungsi dengan default argumen
+
+
+def printdata(usia, nama="Member"): # Default argumen diletakan setelah argumen yang tidak memiliki nilai default
+    print("Nama: ", nama)
+    print("Usia: ", usia)
+
+
+printdata(nama="Jawir", usia=25)
+printdata(usia=24)
