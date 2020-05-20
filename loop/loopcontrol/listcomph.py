2967f73a6f31889e7febee3db8abfab26fb4882a
haryadwi <haryadwi@gmail.com>
2020-05-20 14:18:00 +0700

    List comph final commit

--- loop/loopcontrol/listcomph.py
+++ loop/loopcontrol/listcomph.py
@@ -30,3 +30,16 @@ list_b = [2, 3, 4, 5]
 common_num = [a for a in list_a for b in list_b if a==b] # Lebih ringkas
 print(common_num)
 
+print('\n')
+
+# Convert all list into lower case
+list_a = ["HeLLo", "WOrld", "In", "PytHon"]
+small_list_a = [_.lower() for _ in list_a]  # Mengecilkan huruf yang ada didalam list
+print(small_list_a)
+
+print('\n')
+
+# Kuis
+list_a = range(1, 10, 2)  # Berisikan urutan angka dimulai dari 1 selisih 2 sampai 9
+x = [[a**2, a**3] for a in list_a] # Setiap angka dipangkat 2 dan 3
+print(x)
