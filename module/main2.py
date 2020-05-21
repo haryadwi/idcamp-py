cd2a6d14dc6258aa21404f9ac8159a7bfe20f28d
haryadwi <haryadwi@gmail.com>
2020-05-21 08:15:26 +0700

    Update main2.py

--- module/main2.py
+++ module/main2.py
@@ -1,4 +1,4 @@
 # Import module hello2.py yang diletakan di lib 'site packages' 
-# Atau gunakan print(sys.path) untuj mengetahui lokasi 'site packages'
+# Atau gunakan print(sys.path) untuk mengetahui lokasi 'site packages'
 import hello2
 
