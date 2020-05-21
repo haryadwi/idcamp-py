29da549584eb2c0ecca11fd588703bb7c44712f9
haryadwi <haryadwi@gmail.com>
2020-05-21 08:08:31 +0700

    Module final commit

--- module/hello2.py
+++
@@ -1,7 +0,0 @@
-# Define a function
-def world():
-  print("Hello, world! from hello2.py")
-
-# Call a function
-
-world()
---
+++ module/main2.py
@@ -0,0 +1,4 @@
+# Import module hello2.py yang diletakan di lib 'site packages' 
+# Atau gunakan print(sys.path) untuj mengetahui lokasi 'site packages'
+import hello2
+
