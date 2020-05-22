980180d5001bdee926fbf6e2947a7d790cb28d7f
haryadwi <haryadwi@gmail.com>
2020-05-23 06:41:23 +0700

    OOP class final commit

--- oop/class.py
+++ oop/class.py
@@ -1,4 +1,4 @@
-class kalkulator:
+class Kalkulator:
 
     def __init__(self, i=12345):
         self.i = i
@@ -6,12 +6,39 @@ class kalkulator:
     def f(self):
         return 'Hello world'
 
+    @classmethod
+    def tambah_angka(cls, angka1, angka2):
+        return '{} + {} = {}'.format(angka1, angka2, angka1 + angka2)
+    
+    @classmethod
+    def kali_angka(cls, angka1, angka2):
+        return '{} x {} = {}'.format(angka1, angka2, angka1 * angka2)
 
-# Membuat instance dari kalkulato
-k = kalkulator(i=1024)
-
+# Membuat instance dari kalkulator
+k = Kalkulator(i=1024)
 # Mencetak atribut i dari objek k
 print(k.i)
 
-# Memanggil metode f dari objek k
-k.f()
+print('\n')
+
+# Pemanggilan dari class tambah angka
+print(Kalkulator.tambah_angka(1, 2))
+
+print('\n')
+
+# Pemanggilan dari objek tambah angka
+k = Kalkulator # Instansiasi dari class Kalkulator
+print(k.tambah_angka(1, 2))
+
+print('\n')
+
+# Pemanggilan static method dari class kali angka
+a = Kalkulator.kali_angka(3, 4)
+print(a)
+
+print('\n')
+
+# Pemanggilan static method dari objek kali angka
+k = Kalkulator()
+a = k.kali_angka(3, 4)
+print(a)
