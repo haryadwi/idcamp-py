0443ed93dad880bfff68065837283bd5776b74ea
haryadwi <haryadwi@gmail.com>
2020-05-23 08:24:36 +0700

    Data structure

---
+++ oop/datstruct.py
@@ -0,0 +1,12 @@
+class Pegawai:
+    pass  # Definisi kelas kosong
+
+
+jaw = Pegawai()  # Membuat Pegawai baru menjadi objek bernama jaw
+
+jaw.nama = 'Mas Jawir'
+jaw.bagian = 'Data Analyst'
+jaw.gaji = '6000'
+
+a = jaw.nama
+print(jaw.nama)
