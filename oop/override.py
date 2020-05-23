1c3a720c81c11b407de44212906ef396b2b22f21
haryadwi <haryadwi@gmail.com>
2020-05-23 07:51:26 +0700

    Fixed Issue

--- oop/override.py
+++ oop/override.py
@@ -1,5 +1,5 @@
 class Kalkulator:
-  ''' Kelas kalkulator sederhana '''
+  ''' Kelas kalkulator sederhana tidak boleh diubah '''
 
   def __init__(self, nilai=0):
     self.nilai = nilai
@@ -16,9 +16,15 @@ class KalkulatorKali(Kalkulator):
   def kali_angka(self, angka1, angka2):
     self.nilai = angka1 * angka2
     return self.nilai
+  
+  '''Override methods untung menghilangkan warning dari metode tambah angka kalkulator sederhana'''
+  def tambah_angka(self, angka1, angka2):
+    self.nilai = angka1 + angka2
+    return self.nilai
 
 # Pemanggilan class kalkulator kali
 kk = KalkulatorKali()
+
 a = kk.kali_angka(2, 3) # Memeliki objek kali angka fitur aseli Kalkulator kali
 print(a, '\n')
 
